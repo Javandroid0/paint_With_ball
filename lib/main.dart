c0659b776f199db38ffbe18a1cca9bead352f3ab
Morteza <mortezaj015@gmail.com>
2023-09-28 08:03:17 +0330

    step 2

--- lib/main.dart
+++ lib/main.dart
@@ -61,9 +61,13 @@ class _GameScreenState extends State<GameScreen> {
   double dotX = 0.0;
   double dotY = 0.0;
   List<Offset> ballPath = [];
-  List<Offset> line_path = [];
-  bool draw = false;
+  List<Offset> linePath = [];
+  List<List<Offset>> paths = [];
   Color selectedColor = Colors.blue;
+  List<Color> colors = [];
+
+  bool draw = false;
+
   @override
   void initState() {
     super.initState();
@@ -76,7 +80,7 @@ class _GameScreenState extends State<GameScreen> {
         ballPath.add(Offset(
             -dotX * 4, dotY * 6)); // Add current position to the ballPath
         if (draw) {
-          line_path.add(Offset(-dotX * 4, dotY * 6));
+          linePath.add(Offset(-dotX * 4, dotY * 6));
         }
       });
     });
@@ -87,12 +91,25 @@ class _GameScreenState extends State<GameScreen> {
       dotX = 0;
       dotY = 0;
       ballPath.clear(); // Clear the ballPath to remove all lines
+      paths.clear();
+      linePath.clear();
     });
   }
 
   void startDraw() {
     draw = true;
-    print(line_path);
+    colors.add(selectedColor);
+    //print(selectedColor);
+    //print(linePathColors);
+  }
+
+  void stopDraw() {
+    draw = false;
+    //linePathColors.removeLast();
+    paths.add(linePath);
+    linePath = [];
+
+    //print(paths);
   }
 
   void openColorPickerDialog(BuildContext context) {
@@ -161,7 +178,8 @@ class _GameScreenState extends State<GameScreen> {
           Expanded(
             child: ballPath.isNotEmpty
                 ? CustomPaint(
-                    painter: BallPainter(ballPath, selectedColor, line_path),
+                    painter: BallPainter(
+                        ballPath, selectedColor, paths, linePath, colors),
                     //child: Container(),
                   )
                 : Container(),
@@ -172,9 +190,13 @@ class _GameScreenState extends State<GameScreen> {
             children: [
               ElevatedButton(
                 onPressed: () {
-                  startDraw();
+                  if (draw == false) {
+                    startDraw();
+                  } else if (draw == true) {
+                    stopDraw();
+                  }
                 },
-                child: const Text('Draw'),
+                child: Text(draw ? 'Stop Drawing' : 'Draw'),
               ),
               const SizedBox(
                 width: 16,
@@ -202,22 +224,61 @@ class _GameScreenState extends State<GameScreen> {
 
 class BallPainter extends CustomPainter {
   final List<Offset> ballPath;
+  final List<List<Offset>> paths;
   final List<Offset> linePath;
   final Color color; // Added color property
+  final List<Color> colors;
 
-  BallPainter(this.ballPath, this.color, this.linePath); // Updated constructor
+  BallPainter(this.ballPath, this.color, this.paths, this.linePath,
+      this.colors); // Updated constructor
 
   @override
   void paint(Canvas canvas, Size size) {
-    _drawLine(canvas);
+    _drawPaths(canvas);
     _drawBall(canvas);
+    _drawLine(canvas);
   }
 
+  void _drawPaths(Canvas canvas) {
+    for (int i = 0; i < paths.length; i++) {
+      final paint = Paint()
+        ..color = colors[i % colors.length] // Use the selected color
+        ..strokeWidth = 4;
+      for (int j = 1; j < paths[i].length; j++) {
+        canvas.drawLine(paths[i][j - 1], paths[i][j], paint);
+      }
+    }
+  }
+
+  // void _drawPaths(Canvas canvas) {
+  //   final paint = Paint()
+  //     ..color = color // Use the selected color
+  //     ..strokeWidth = 4;
+  //   for (final path in paths) {
+  //     for (int i = 1; i < path.length; i++) {
+  //       canvas.drawLine(path[i - 1], path[i], paint);
+  //     }
+  //   }
+  // }
+
+  // void _drawPaths(Canvas canvas) {
+  //   for (int i = 0; i < paths.length; i++) {
+  //     final paint = Paint()
+  //       ..color = linePathColors[i] // Use the color for the current linePath
+  //       ..strokeWidth = 4;
+
+  //     final path = paths[i];
+  //     for (int j = 1; j < path.length; j++) {
+  //       canvas.drawLine(path[j - 1], path[j], paint);
+  //     }
+  //   }
+  // }
+
   void _drawLine(Canvas canvas) {
-    final paint = Paint()
-      ..color = color // Use the selected color
-      ..strokeWidth = 4;
     for (int i = 1; i < linePath.length; i++) {
+      final paint = Paint()
+        ..color = colors[i % colors.length] // Use the selected color
+        ..strokeWidth = 4;
       canvas.drawLine(linePath[i - 1], linePath[i], paint);
     }
   }
